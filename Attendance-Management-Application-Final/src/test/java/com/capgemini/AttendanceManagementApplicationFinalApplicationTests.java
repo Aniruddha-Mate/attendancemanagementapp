73623e157695d3bdfc0ef8e3d622a31ef074e43c
aniru <aniru@LAPTOP-LEGION5i>
2021-04-18 21:45:47 +0530

    save

--- Attendance-Management-Application-Final/src/test/java/com/capgemini/AttendanceManagementApplicationFinalApplicationTests.java
+++ Attendance-Management-Application-Final/src/test/java/com/capgemini/AttendanceManagementApplicationFinalApplicationTests.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.capgemini.entity.AttendanceEntity;
 import com.capgemini.entity.CourseEntity;
 import com.capgemini.entity.FacultyEntity;
